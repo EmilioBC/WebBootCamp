2686033e003ac31f6a88d8d13bb5e6daf19a424e
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-14 06:29:41 +0000

    Separation

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,5 @@
 var thirdInList = document.getElementsByClassName("item")[2].innerHTML = "Beth";
 var listLink = document.querySelector("#list a").style.color = "Red";
+var fontSize = document.querySelector(".btn").classList.add("huge");
+var bgColor = document.querySelector(".btn").classList.add("bgYellow");
 
-
-console.log(thirdInList);
