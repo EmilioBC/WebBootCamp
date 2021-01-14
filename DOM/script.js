2547aaa77997a8ed0b4aa0f94dded2188ab01249
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-01-14 05:31:47 +0000

    Elements

---
+++ DOM/script.js
@@ -0,0 +1,5 @@
+var thirdInList = document.getElementsByClassName("item")[2].innerHTML = "Beth";
+var listLink = document.querySelector("#list a").style.color = "Red";
+
+
+console.log(thirdInList);
