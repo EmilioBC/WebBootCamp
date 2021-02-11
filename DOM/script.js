73c4f892f115f9d740ccfc3cfd41346fc55f0cac
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-02-11 02:01:34 +0000

    DOMend

--- DOM/script.js
+++ DOM/script.js
@@ -2,4 +2,6 @@ var thirdInList = document.getElementsByClassName("item")[2].innerHTML = "Beth";
 var listLink = document.querySelector("#list a").style.color = "Red";
 var fontSize = document.querySelector(".btn").classList.add("huge");
 var bgColor = document.querySelector(".btn").classList.add("bgYellow");
-
+document.querySelector("h1").innerHTML = "Hello, Love, Goodbye";
+document.querySelector("a").setAttribute("href", "www.bing.com");
+document.querySelector("a").innerHTML = "Bing";
