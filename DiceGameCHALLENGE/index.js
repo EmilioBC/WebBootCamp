cd7eb75016db1920f058bc591a91dccb102f1c24
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-02-11 06:23:41 +0000

    stuck

---
+++ DiceGameCHALLENGE/index.js
@@ -0,0 +1,22 @@
+var randomNumber = Math.floor((Math.random() * 6) + 1);
+// var player1 = document.querySelector("img1");
+// var player2 = document.querySelector("img2");
+
+// player1.setAttribute("src", `images/dice${randomNumber}.png`)
+
+document.querySelector("img")[0].setAttribute("src", `images/dice${randomNumber}.png`);
+
+
+
+
+
+// function rollDice() {
+//   document.querySelector("img").setAttribute("src", `images/dice${randomNumber1}.png`);
+// }
+
+// console.log(randomNumber1);
+
+// function myFunction() {
+//   var x = Math.floor((Math.random() * 10) + 1);
+//   document.getElementById("demo").innerHTML = x;
+// 
